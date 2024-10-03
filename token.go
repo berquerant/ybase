4b90866a66f32608375a02a53b2eb136d8c0ca9d
berquerant <sin@sin-pro.local>
2024-10-03 23:02:21 +0900

    Fix receiver

--- token.go
+++ token.go
@@ -35,7 +35,7 @@ func (s token) Value() string  { return s.v }
 func (s token) Start() Pos     { return s.start }
 func (s token) End() Pos       { return s.end }
 func (s token) String() string { return fmt.Sprintf("%d,%s", s.t, s.v) }
-func (s *token) MarshalJSON() ([]byte, error) {
+func (s token) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]any{
 		"type":  s.t,
 		"value": s.v,
